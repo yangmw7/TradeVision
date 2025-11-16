6aaca223c0bac531ffe04b40a1f9db0825240e90
yangmw7 <yangmw7@naver.com>
2025-11-16 12:10:32 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ tradevision/src/main/java/com/example/tradevision/TradeVisionApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeVisionApplication.class, args);
+	}
+
+}
---
+++ tradevision/src/test/java/com/example/tradevision/TradeVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
